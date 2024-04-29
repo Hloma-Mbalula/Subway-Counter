aa23b70609f5cffafca9b968e4178a416707c238
Hloma Mbalula <hlomambalula88@gmail.com>
2024-04-29 20:04:18 +0200

    removing the add function

--- index.js
+++ index.js
@@ -15,29 +15,6 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
-function add() {
-    let prevEntries = (saveEl.textContent += countStr)
-    //Im trying to break whatever is on saveEl into an arry
-    let arr = prevEntries.split(" ")
-    //[5, '-', 5, '-',6]
-    //Once its in an array, I want to push the numbers only, into a new arr
-    let newArr = []
-    let total = 0
-    for(let i = 0; i< arr.length; i++){
-        if(isNaN(arr[i]) === true){
-            arr[i].push(newArr)
-            for(let j = 0; j < newArr.length; j++){
-                total += newArr[j];
-                
-            }
-        }
-        return total;
-        }
-        
-    
-     totalEl.textContent += total
-    
-   
-}
+
 
 console.log("Let's count people on the subway!")
